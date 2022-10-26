10154c841aae6d66c2adfb563872210d70219e29
mingjue <zhening.czn@alibaba-inc.com>
2022-10-26 19:34:59 +0800

    add web.xml

--- src/main/java/me/yifeiyuan/GreetingServlet.java
+++ src/main/java/me/yifeiyuan/GreetingServlet.java
@@ -9,18 +9,21 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 
-@WebServlet("/greeting")
+//@WebServlet("/greeting")
+
+/**
+ * 使用了 web.xml 配置
+ */
 public class GreetingServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         // 设置响应类型:
         resp.setContentType("text/html");
         // 获取输出流:
         PrintWriter pw = resp.getWriter();
         // 写入响应:
-        pw.write("<h1>GreetingServlet</h1>");
+        pw.write("<h1>Greeting Servlet</h1>");
         // 最后不要忘记flush强制输出:
         pw.flush();
     }
